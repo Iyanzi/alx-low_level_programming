a17d3d61bfcdfe086512c978c988b8695a0d9aa6
Iyanzi <fakoredeiyanuoluwa@yahoo.com>
2022-12-13 16:09:48 -0800

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,13 @@
 
 /**
  * print_alphabet - prints the alpha in lowercase
- *
- * Return: Always return 0.
- *
  */
-
 void print_alphabet(void)
 {
-	char letter;
-	for (lc = 'a'; lc<= 'z'; lc++)
-	{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
 		_putchar(lc)
-	}
+
 	_putchar('\n');
-	return(0);
 }
